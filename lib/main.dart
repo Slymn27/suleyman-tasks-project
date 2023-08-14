00c3b47b433f2574943303122eb355ae1343d3a5
Slymn27 <suleymankiskac27@gmail.com>
2023-08-14 20:21:20 +0300

    created classes files

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
+import 'classes.dart';
 
 void main() {
   runApp(const MyApp());
